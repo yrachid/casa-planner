9174eae6209488966f64afdbccf3e8d85cb5ab50
othman853 <anuar.yasser@gmail.com>
2016-08-19 13:42:56 -0300

    Adding link field to loja model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -11,6 +11,7 @@ class Loja(Base):
 
     id = db.Column(db.Integer(), primary_key=True)
     nome = db.Column(db.String())
+    link = db.Column(db.String())
 
     def __repr__(self):
         return self.nome
