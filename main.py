92fe3e0444e7700f50a3a4a4c459301b41728d57
othman853 <anuar.yasser@gmail.com>
2016-08-18 23:59:18 -0300

    Let the stack trace be free

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ from app.views.users.list import UserListView
 from app.models import db
 import os
 
+app = factory(os.environ.get('CASAPLANNER_CONFIG', 'DevelopmentConfig'))
+
 if __name__ == '__main__':
-
-    try:
-        app = factory(os.environ.get('SYRUP_CONFIG', 'DevelopmentConfig'))
-
-        app.run()
-    except Exception as error:
-        print(str(error))
+    app.run()
