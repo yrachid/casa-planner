0b9994732511ba191b67094134e58038fec8eae2
othman853 <anuar.yasser@gmail.com>
2016-08-19 14:54:05 -0300

    Fixing deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,9 +7,9 @@ ssh -i $DEPLOY_KEY root@$DEPLOY_HOST "apt-get install -y unzip"
 ssh -i $DEPLOY_KEY $DEPLOY_USER@$DEPLOY_HOST 'mkdir -p ~/build'
 ssh -i $DEPLOY_KEY $DEPLOY_USER@$DEPLOY_HOST 'mkdir -p ~/app'
 ssh -i $DEPLOY_KEY $DEPLOY_USER@$DEPLOY_HOST 'rm -rf ~/build/*'
-scp -i $DEPLOY_KEY "$PACK_DESTINATION/$PACKED_NAME" $DEPLOY_USER@$DEPLOY_HOST:/home/$DEPLOY_USER/build/
-ssh -i $DEPLOY_KEY $DEPLOY_USER@$DEPLOY_HOST "unzip ~/build/$PACKED_NAME -d ~/app"
-ssh -i $DEPLOY_KEY root@$DEPLOY_HOST "sh /home/$DEPLOY_USER/build/bin/prod-setup.sh"
+scp -i $DEPLOY_KEY "$PACK_DESTINATION/$PACK_NAME" $DEPLOY_USER@$DEPLOY_HOST:/home/$DEPLOY_USER/build/
+ssh -i $DEPLOY_KEY $DEPLOY_USER@$DEPLOY_HOST "unzip ~/build/$PACK_NAME -d ~/app"
+ssh -i $DEPLOY_KEY root@$DEPLOY_HOST "sh /home/$DEPLOY_USER/app/bin/prod-setup.sh"
 
 #pip3 install -r /home/$DEPLOY_USER/build/requirements/production.txt
 
