1f1d7bf8e736283401d953019755d347595f0e0f
othman853 <anuar.yasser@gmail.com>
2016-08-19 15:16:18 -0300

    Adding more steps to deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,11 +9,10 @@ ssh -i $DEPLOY_KEY $DEPLOY_USER@$DEPLOY_HOST 'mkdir -p ~/app'
 ssh -i $DEPLOY_KEY $DEPLOY_USER@$DEPLOY_HOST 'rm -rf ~/build/*'
 scp -i $DEPLOY_KEY "$PACK_DESTINATION/$PACK_NAME" $DEPLOY_USER@$DEPLOY_HOST:/home/$DEPLOY_USER/build/
 ssh -i $DEPLOY_KEY $DEPLOY_USER@$DEPLOY_HOST "unzip ~/build/$PACK_NAME -d ~/app"
+
 ssh -i $DEPLOY_KEY root@$DEPLOY_HOST "sh /home/$DEPLOY_USER/app/bin/prod-setup.sh"
 
-#pip3 install -r /home/$DEPLOY_USER/build/requirements/production.txt
-
-#rm -rf /var/www/html/*
-#cp etc/production/casaplanner.wsgi /var/www/html/
-
-#service apache2 restart
+ssh -i $DEPLOY_KEY $DEPLOY_USER@$DEPLOY_HOST "pip3 install -r ~/app/requirements/production.txt"
+ssh -i $DEPLOY_KEY root@$DEPLOY_HOST "rm -rf /var/www/html/*"
+ssh -i $DEPLOY_KEY root@$DEPLOY_HOST "cp /home/$DEPLOY_USER/app/etc/production/casaplanner.wsgi /var/www/html/"
+ssh -i $DEPLOY_KEY root@$DEPLOY_HOST "service apache2 restart"
